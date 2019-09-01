695d6147b49fe1abd981e4c6e1b3b2e0770dea81
Fabio Papa <fabtheman@gmail.com>
2019-08-31 17:15:19 -0700

    Add rudimentry graph defaults, with the ability to pass in overrides

--- demo/js/bar-chart.js
+++ demo/js/bar-chart.js
@@ -1,3 +1,17 @@
 const drawBarChart = function (data, options, element) {
-  element.html("The data being worked with is " + data + "!");
+  const max = Math.max.apply(Math, data);
+  let graph = $("<div class='graph'></div>");
+
+  element.css({
+    "width": options.width || "500px",
+    "height": options.height || "300px"
+  });
+
+  graph.css({
+    "height": "100%",
+    "border-left": options.lineStyle || "1px solid black",
+    "border-bottom": options.lineStyle || "1px solid black"
+  });
+
+  element.append(graph);
 };
