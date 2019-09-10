1c8548ee2ba24870714fbbe66d6b2c3b3873c11f
Fabio Papa <fabtheman@gmail.com>
2019-09-10 15:20:04 -0700

    Add a second standard chart to the demo page, showing options set

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,1 +1,11 @@
 drawBarChart([ 1, 2, 3, 4, 5 ], {}, $("#basic"));
+
+drawBarChart([ 1, 2, 3, 4, 5 ], {
+  title: "Dunder Mifflin&mdash;Quabity Assuance Over Time",
+  height: "500px",
+  width: "800px",
+  barSpacing: "5%",
+  barColor: "#ace",
+  showTicks: true,
+  yAxisName: "Blunders (per month)"
+}, $("#options"));
