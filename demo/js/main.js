85458c7ec48203da1bee5f318d7f1285a2c9b2f7
Fabio Papa <fabtheman@gmail.com>
2019-09-10 16:09:45 -0700

    Add a third chart to the demo page, this time a stacked chart

--- demo/js/main.js
+++ demo/js/main.js
@@ -9,3 +9,17 @@ drawBarChart([ 1, 2, 3, 4, 5 ], {
   showTicks: true,
   yAxisName: "Blunders (per month)"
 }, $("#options"));
+
+drawBarChart({
+  "Northwest": { "Qtr 1": 3767341, "Qtr 2": 3298694, "Qtr 3": 2448772, "Qtr 4": 1814281 },
+  "Northeast": { "Qtr 1": 2857163, "Qtr 2": 3607148, "Qtr 3": 1857156, "Qtr 4": 1983931 },
+  "Central": { "Qtr 1": 3677108, "Qtr 2": 3205014, "Qtr 3": 2390120, "Qtr 4": 1762757 },
+  "Southwest": { "Qtr 1": 2851432, "Qtr 2": 3571335, "Qtr 3": 1932932, "Qtr 4": 1653192 }
+}, {
+  title: "Sales by Division and by Quarter&mdash;2018",
+  yAxisName: "Dollars (CAD)",
+  xAxisName: "Division",
+  showTicks: true,
+  barColor: [ "#ace", "#eac", "#cea", "#cae" ],
+  legendPosition: { top: "2em", right: "100px" }
+}, $("#stacked-extra"));
